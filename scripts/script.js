9df93775964fca522cba38453b6154163aaee607
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-01 15:20:48 +0530

    Added getting advice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log("Injected the script.");
+const advice = document.querySelector("#advice");
+const button = document.querySelector("#button");
+
+button.addEventListener("click", () => {
+  updateQuote();
+});
+
+const updateQuote = async () => {
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const { slip } = await res.json();
+
+    advice.innerHTML = slip.advice;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+updateQuote();
