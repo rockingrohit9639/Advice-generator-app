dea61b068e3555d7afb974f326f11abd81c05291
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-01 17:05:01 +0530

    Added advice id

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const advice = document.querySelector("#advice");
 const button = document.querySelector("#button");
+const aid = document.querySelector("#aid");
 
 button.addEventListener("click", () => {
   updateQuote();
@@ -9,8 +10,8 @@ const updateQuote = async () => {
   try {
     const res = await fetch("https://api.adviceslip.com/advice");
     const { slip } = await res.json();
-
     advice.innerHTML = slip.advice;
+    aid.innerHTML = slip.id;
   } catch (err) {
     console.log(err);
   }
